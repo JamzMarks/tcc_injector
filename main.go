f1a91c6fd2d40d0caf048e20543de0124e154e14
James Marques <jamzmarks@gmail.com>
2025-10-10 07:50:58 -0300

    feat: message waiter

--- main.go
+++ main.go
@@ -8,64 +8,66 @@ import (
 	"time"
 )
 
-// type EdgeData struct {
-// 	DeviceID string `json:"deviceId"`
-// 	Location struct {
-// 		To   int `json:"to"`
-// 		From int `json:"from"`
-// 	} `json:"location"`
-// 	Data struct {
-// 		Confiability float32 `json:"confiability"`
-// 		Flow         float32 `json:"flow"`
-// 	} `json:"data"`
-// }
-
 func main() {
-	amqpURL := os.Getenv("AMQP_URL")
-	queueName := os.Getenv("QUEUE_NAME")
-	if amqpURL == "" || queueName == "" {
-		log.Fatal("Variáveis de ambiente do broker não configuradas")
-	}
+    amqpURL := os.Getenv("AMQP_URL")
+    queueName := os.Getenv("QUEUE_NAME")
+    if amqpURL == "" || queueName == "" {
+        log.Fatal("Variáveis de ambiente do broker não configuradas")
+    }
 
-	uri := os.Getenv("GRAPH_DB_URI")
-	username := os.Getenv("GRAPH_DB_USERNAME")
-	password := os.Getenv("GRAPH_DB_PASSWORD")
-	if uri == "" || username == "" || password == "" {
-		log.Fatal("Variáveis de ambiente do banco de grafos não configuradas")
-	}
+    uri := os.Getenv("GRAPH_DB_URI")
+    username := os.Getenv("GRAPH_DB_USERNAME")
+    password := os.Getenv("GRAPH_DB_PASSWORD")
+    if uri == "" || username == "" || password == "" {
+        log.Fatal("Variáveis de ambiente do banco de grafos não configuradas")
+    }
 
-	// Neo4j
-	injector := services.NewInjectorService(uri, username, password)
-	defer injector.Close()
-	injector.TestConnection()
+    // Neo4j
+    injector := services.NewInjectorService(uri, username, password)
+    defer injector.Close()
+    injector.TestConnection()
 
-	// RabbitMQ
-	rabbit, err := services.NewRabbitService(amqpURL, queueName)
-	if err != nil {
-		log.Fatalf("Erro ao conectar RabbitMQ: %v", err)
-	}
-	defer rabbit.Close()
+    // RabbitMQ
+    rabbit, err := services.NewRabbitService(amqpURL, queueName)
+    if err != nil {
+        log.Fatalf("Erro ao conectar RabbitMQ: %v", err)
+    }
+    defer rabbit.Close()
 
-	ch := make(chan types.EdgeData, 100)
+    ch := make(chan types.EdgeData, 100)
 
-	go MessageWaiter(ch)
-	go EdgeListBuilder(ch, 5*time.Second)
+    go MessageWaiter(ch, rabbit)
+    go EdgeListBuilder(ch, 5*time.Second)
 
-	select {}
+    select {} 
 }
 
-func MessageWaiter(ch chan<- types.EdgeData) types.EdgeData {
-	var e types.EdgeData
-	e.DeviceID = "sensor-01"
-	e.Location.From = 1
-	e.Location.To = 2
-	e.Data.Confiability = 0.93
-	e.Data.Flow = 10.5
-	return e
+
+func MessageWaiter(ch chan<- types.EdgeData, rabbit *services.RabbitService) {
+    err := rabbit.Consume(func(body []byte) error {
+        var e types.EdgeData
+        if err := json.Unmarshal(body, &e); err != nil {
+            return fmt.Errorf("erro ao decodificar mensagem: %w", err)
+        }
+
+        select {
+        case ch <- e:
+        default:
+            log.Printf("canal interno cheio, descartando mensagem de %s -> %d", e.DeviceID, e.Location.To)
+        }
+
+        return nil
+    })
+
+    if err != nil {
+        log.Printf("erro ao iniciar consumo do RabbitMQ: %v", err)
+    }
+
+    log.Println("MessageWaiter iniciado e escutando mensagens...")
 }
 
 // Acumulador de Edges
-func EdgeListBuilder(ch <-chan types.EdgeData, timeout time.Duration) {
+func EdgeListBuilder(ch <-chan types.EdgeData, timeout time.Duration, injector *services.InjectorService) {
 	buffer := make([]types.EdgeData, 0, 50)
 	timer := time.NewTimer(timeout)
 
@@ -75,7 +77,9 @@ func EdgeListBuilder(ch <-chan types.EdgeData, timeout time.Duration) {
 			buffer = append(buffer, edge)
 
 			if len(buffer) >= cap(buffer) {
-				Injector(buffer)
+				dataCopy := make([]types.EdgeData, len(buffer))
+                copy(dataCopy, buffer)
+                go injector.InjectBuffer(dataCopy)
 				buffer = buffer[:0]
 				if !timer.Stop() {
 					<-timer.C
@@ -85,7 +89,9 @@ func EdgeListBuilder(ch <-chan types.EdgeData, timeout time.Duration) {
 
 		case <-timer.C:
 			if len(buffer) > 0 {
-				Injector(buffer)
+				dataCopy := make([]types.EdgeData, len(buffer))
+                copy(dataCopy, buffer)
+				go injector.InjectBuffer(dataCopy)
 				buffer = buffer[:0]
 			}
 			timer.Reset(timeout)
@@ -93,6 +99,3 @@ func EdgeListBuilder(ch <-chan types.EdgeData, timeout time.Duration) {
 	}
 }
 
-func Injector(data []EdgeData) {
-
-}
--- services/broker_service.go
+++ services/broker_service.go
@@ -1,82 +1,82 @@
-package services
+	package services
 
-import (
-	"log"
+	import (
+		"log"
 
-	amqp "github.com/rabbitmq/amqp091-go"
-)
-
-type RabbitService struct {
-	conn    *amqp.Connection
-	channel *amqp.Channel
-	queue   string
-}
-
-func NewRabbitService(amqpURL, queueName string) (*RabbitService, error) {
-	conn, err := amqp.Dial(amqpURL)
-	if err != nil {
-		return nil, err
-	}
-
-	ch, err := conn.Channel()
-	if err != nil {
-		conn.Close()
-		return nil, err
-	}
-
-	_, err = ch.QueueDeclare(
-		queueName,
-		true,  // durable
-		false, // autoDelete
-		false, // exclusive
-		false, // noWait
-		nil,   // args
+		amqp "github.com/rabbitmq/amqp091-go"
 	)
-	if err != nil {
-		ch.Close()
-		conn.Close()
-		return nil, err
+
+	type RabbitService struct {
+		conn    *amqp.Connection
+		channel *amqp.Channel
+		queue   string
 	}
 
-	return &RabbitService{
-		conn:    conn,
-		channel: ch,
-		queue:   queueName,
-	}, nil
-}
-
-// Close fecha a conexão e o canal
-func (r *RabbitService) Close() {
-	if r.channel != nil {
-		r.channel.Close()
-	}
-	if r.conn != nil {
-		r.conn.Close()
-	}
-}
-
-// Consume inicia o consumo de mensagens
-func (r *RabbitService) Consume(handler func(body []byte) error) error {
-	msgs, err := r.channel.Consume(
-		r.queue,
-		"",    // consumer
-		true,  // autoAck
-		false, // exclusive
-		false, // noLocal
-		false, // noWait
-		nil,   // args
-	)
-	if err != nil {
-		return err
-	}
-
-	go func() {
-		for msg := range msgs {
-			if err := handler(msg.Body); err != nil {
-				log.Printf("Erro ao processar mensagem: %v", err)
-			}
+	func NewRabbitService(amqpURL, queueName string) (*RabbitService, error) {
+		conn, err := amqp.Dial(amqpURL)
+		if err != nil {
+			return nil, err
 		}
-	}()
 
-	return nil
-}
+		ch, err := conn.Channel()
+		if err != nil {
+			conn.Close()
+			return nil, err
+		}
+
+		_, err = ch.QueueDeclare(
+			queueName,
+			true,  // durable
+			false, // autoDelete
+			false, // exclusive
+			false, // noWait
+			nil,   // args
+		)
+		if err != nil {
+			ch.Close()
+			conn.Close()
+			return nil, err
+		}
+
+		return &RabbitService{
+			conn:    conn,
+			channel: ch,
+			queue:   queueName,
+		}, nil
+	}
+
+	// Close fecha a conexão e o canal
+	func (r *RabbitService) Close() {
+		if r.channel != nil {
+			r.channel.Close()
+		}
+		if r.conn != nil {
+			r.conn.Close()
+		}
+	}
+
+	// Consume inicia o consumo de mensagens
+	func (r *RabbitService) Consume(handler func(body []byte) error) error {
+		msgs, err := r.channel.Consume(
+			r.queue,
+			"",    // consumer
+			true,  // autoAck
+			false, // exclusive
+			false, // noLocal
+			false, // noWait
+			nil,   // args
+		)
+		if err != nil {
+			return err
+		}
+
+		go func() {
+			for msg := range msgs {
+				if err := handler(msg.Body); err != nil {
+					log.Printf("Erro ao processar mensagem: %v", err)
+				}
+			}
+		}()
+
+		return nil
+	}
